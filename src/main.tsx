c64840c645d4d68dac9895673f4d3744086e5151
helcio <h.andre@giap.pl>
2023-07-19 12:38:04 +0100

    start creating rating component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./main.css";
+// import "./main.css";
+import Rating from "./Rating.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Rating />
   </React.StrictMode>
 );
